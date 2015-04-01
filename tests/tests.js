54fb0cc13bd99ddc00be21418eb5a66cb5582a4c
Jason Chase <jasonpchase@gmail.com>
2015-04-01 15:34:30 -0400

    Convert manual tests to use Cordova plugin test framework

--- tests/tests.js
+++ tests/tests.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-registerManualTests('chrome.bluetooth', function(rootEl, addButton) {
+exports.defineManualTests = function(rootEl, addButton) {
   addButton('Test getAdapter State', function() {
     chrome.bluetooth.getAdapterState(function(adapter) {
       logger(JSON.stringify(adapter));
@@ -53,4 +53,4 @@ registerManualTests('chrome.bluetooth', function(rootEl, addButton) {
     });
   });
 
-});
+};
