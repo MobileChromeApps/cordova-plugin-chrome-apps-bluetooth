7b881accfd84530243e2bcf99a09014ab4f4cb5a
Jason Chase <jasonpchase@gmail.com>
2015-04-13 15:16:17 -0400

    Fix references specific to ChromeSpec - Use console.log instead of `logger` from ChromeSpec app

--- tests/tests.js
+++ tests/tests.js
@@ -5,51 +5,51 @@
 exports.defineManualTests = function(rootEl, addButton) {
   addButton('Test getAdapter State', function() {
     chrome.bluetooth.getAdapterState(function(adapter) {
-      logger(JSON.stringify(adapter));
+      console.log(JSON.stringify(adapter));
     });
   });
 
   addButton('Add Events Listener', function() {
     chrome.bluetooth.onAdapterStateChanged.addListener(function(adapter) {
-      logger(JSON.stringify(adapter));
+      console.log(JSON.stringify(adapter));
     });
     chrome.bluetooth.onDeviceAdded.addListener(function(device) {
-      logger('Device added:');
-      logger(JSON.stringify(device));
+      console.log('Device added:');
+      console.log(JSON.stringify(device));
     });
     chrome.bluetooth.onDeviceChanged.addListener(function(device) {
-      logger('Device changed:');
-      logger(JSON.stringify(device));
+      console.log('Device changed:');
+      console.log(JSON.stringify(device));
     });
     chrome.bluetooth.onDeviceRemoved.addListener(function(device) {
-      logger('Device removed:');
-      logger(JSON.stringify(device));
+      console.log('Device removed:');
+      console.log(JSON.stringify(device));
     });
   });
 
   addButton('Start discovering', function() {
     chrome.bluetooth.startDiscovery(function() {
-      logger('start discovery');
+      console.log('start discovery');
     });
   });
 
   addButton('Stop discovering', function() {
     chrome.bluetooth.stopDiscovery(function() {
-      logger('stop discovery');
+      console.log('stop discovery');
     });
   });
 
   addButton('Listing known devices', function() {
     chrome.bluetooth.getDevices(function(devices) {
       for (var i = 0; i < devices.length; i++) {
-        logger(JSON.stringify(devices[i]));
+        console.log(JSON.stringify(devices[i]));
       }
     });
   });
 
   addButton('Get device Info with unknown address', function() {
     chrome.bluetooth.getDevice('64:A3:CB:3A:87:65', function(device) {
-      logger(JSON.stringify(device));
+      console.log(JSON.stringify(device));
     });
   });
 
